53fc267b5be08567290f5cd878d7c492b6b16678
Kamal Mango <kamal.mango7@gmail.com>
2022-01-14 16:16:28 +0100

    build timer feature

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Platform } from 'react-native';
 import { Focus } from './src/features/focus/Focus';
+import { Timer } from './src/features/timer/Timer';
+import { colors } from './src/utils/colors';
+import { spacing } from './src/utils/sizes';
 
 export default function App() {
-  const [focusSubject, setFocusSubject] = useState(null);
+  const [focusSubject, setFocusSubject] = useState('Gardening');
 
   return (
     <View style={styles.container}>
       {focusSubject ? (
-        <Text>Here is where Im going to build a timer</Text>
+        <Timer focusSubject={focusSubject} />
       ) : (
         <Focus addSubject={setFocusSubject} />
       )}
-      <Text>{focusSubject}</Text>
     </View>
   );
 }
@@ -20,6 +22,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#252250'
+    paddingTop: Platform.OS === 'ios' ? spacing.md : spacing.lg,
+    backgroundColor: colors.darkBlue
   },
 });
---
+++ src/components/Countdown.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { colors } from '../utils/colors';
+import { fontSizes } from '../utils/sizes';
+
+const minutesToMillis = (min) => min * 1000 * 60;
+const formatTime = (time) => time < 10 ? `0${time}` : time;
+
+export const Countdown = ({
+  minutes = 20,
+  isPaused,
+}) => {
+  const interval = React.useRef(null);
+
+  const countDown = () => {
+    setMillis((time) => {
+      if (time === 0) {
+        return time;
+      }
+      const timeLeft = time - 1000;
+      return timeLeft;
+    })
+  }
+  const [millis, setMillis] = useState(minutesToMillis(minutes));
+
+  useEffect(() => {
+    if (isPaused) {
+      return;
+    }
+    interval.current = setInterval(countDown, 1000);
+
+    return () => clearInterval(interval.current)
+  }, [isPaused])
+
+  const minute = Math.floor(millis/ 1000/ 60) % 60;
+  const seconds = Math.floor(millis/ 1000) % 60;
+
+  return (
+    <Text style={styles.text}>{formatTime(minute)}:{formatTime(seconds)}</Text>
+  )
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: fontSizes.xxxl,
+    fontWeight: 'bold',
+    color: colors.white,
+    padding: fontSizes.lg,
+    backgroundColor: 'rgba(94, 132, 226, 0.3)' // transparent background
+  }
+})
