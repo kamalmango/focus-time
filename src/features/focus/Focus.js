c03dcafede71e0104f262a8f7a103511d888dbd6
Kamal Mango <kamal.mango7@gmail.com>
2022-01-14 13:17:48 +0100

    add button functionality

--- src/features/focus/Focus.js
+++ src/features/focus/Focus.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { TextInput } from 'react-native-paper';
 import { RoundedButton } from '../../components/RoundedButton';
 
 export const Focus = ({ addSubject }) => {
+  const [tmpItem, setTmpItem] = useState(null)
+
   return (
     <View style={styles.container}>
       <View style={styles.titleContainer}>
         <Text style={styles.title}>What would you like to focus on?</Text>
         <View style={styles.inputContainer}>
-          <TextInput style={{ flex: 1, marginRight: 20 }} />
-          <RoundedButton size={50} title='+' onPress={() => null} />
+          <TextInput 
+            style={{ flex: 1, marginRight: 20 }}
+            onSubmitEditing={
+              ({ nativeEvent }) => {
+                setTmpItem(nativeEvent.text)
+              }}
+          />
+          <RoundedButton size={50} title='+' onPress={() => addSubject(tmpItem)} />
         </View>
       </View>
     </View>
